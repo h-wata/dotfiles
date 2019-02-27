ff6a27e0fe43fbf49a6808b2bb73a21a0baf0aff
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2019-02-27 20:29:22 +0900

    vim8対応

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-sudo apt-get -y install vim-gnome-py2
+
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt -y update
+sudo apt -y install vim
 sudo apt-get -y install flake8
 sudo apt-get -y install clang-format-3.6
 sudo apt-get -y install python-jedi
 sudo apt-get -y install python-pip
 pip install autopep8
+pip install cpplint
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd) #cd current directory
 ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
