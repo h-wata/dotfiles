2471f3374c3b7b8db7ec2b92d6eb5bf84b392ae6
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2020-03-20 01:52:44 +0900

    fix script

--- fzf.sh
+++ fzf.sh
@@ -11,9 +11,9 @@ add_bashrc() {
 add_bashrc "export FZF_DEFAULT_OPTS='--height 40% --reverse --border'"
 add_bashrc "export FZF_CTRL_T_COMMAND='fd -t f --hidden --follow -d 5 -E .git'"
 add_bashrc "export FZF_CTRL_T_OPTS='--preview \"bat  --color=always --style=header,grid --line-range :100 {}\"'"
-wget https://github.com/sharkdp/fd/releases/download/v7.4.0/fd-musl_7.4.0_amd64.deb  ./
-curl://github.com/sharkdp/bat/releases/download/v0.12.1/bat-musl_0.12.1_amd64.deb  ./
-sudo dpkg -i ./fd-musl_7.4.0_amd64.deb bat-musl_0.12.1_amd64.deb
+wget https://github.com/sharkdp/fd/releases/download/v7.4.0/fd-musl_7.4.0_amd64.deb  /tmp/
+wget https://github.com/sharkdp/bat/releases/download/v0.12.1/bat-musl_0.12.1_amd64.deb  /tmp/
+sudo dpkg -i  /tmp/fd-musl_7.4.0_amd64.deb /tmp/bat-musl_0.12.1_amd64.deb
 cd ~/work
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
