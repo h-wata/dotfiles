52dc1d128ad04d96f79e2ae7fa2b6cc6bc5a04e4
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2020-09-16 18:48:09 +0900

    fix for ubuntu18

--- gitconfiglink.sh
+++ gitconfiglink.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd) #cd current directory
-sudo apt-get install -y libgnome-keyring-dev 
-cd /usr/share/doc/git/contrib/credential/gnome-keyring
-sudo make
 
 echo $SCRIPT_DIR
 ln -sf $SCRIPT_DIR/gitconfig ~/.gitconfig
