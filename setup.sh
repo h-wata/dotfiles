7b5fbf99edc3bb0125becc9e3618243b41199713
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2020-09-19 12:25:20 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd) #cd current directory
+add_bashrc() {
+  added=$(grep "$1" ~/.bashrc | wc -l)
+  if [ $added -gt 0 ]; then
+    cecho $yellow "'$1' is already exist in ~/.bashrc"
+  else
+    echo "$1" >> ~/.bashrc
+    cecho $cyan "'$1' was added to ~/.bashrc"
+  fi
+}
+add_bashrc "export FZF_DEFAULT_OPTS='--height 40% --reverse --border'"
+add_bashrc "export FZF_CTRL_T_COMMAND='fd -t f --hidden --follow -d 5 -E .git'"
+add_bashrc "export FZF_CTRL_T_OPTS='--preview \"bat  --color=always --style=header,grid --line-range :100 {}\"'"
 
 bash ./dotfilesLink.sh
 bash ./gitconfiglink.sh
-bash ./fzf.sh
