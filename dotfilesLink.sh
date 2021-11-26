d628f1168e96dc7d9fee26c05bf8618699705b30
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2021-11-26 10:44:10 +0900

    fix setup script

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,11 +5,8 @@ pip install cpplint
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd) #cd current directory
 mkdir ~/.vim
-mkdir -p ~/.config/i3
 ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
 ln -sf $SCRIPT_DIR/.screenrc ~/.screenrc
 ln -sf $SCRIPT_DIR/ftdetect  ~/.vim
 ln -sf $SCRIPT_DIR/bundle ~/.vim
 ln -sf $SCRIPT_DIR/snippets ~/.vim
-ln -sf $SCRIPT_DIR/i3wm/config ~/.config/i3/
-ln -sf $SCRIPT_DIR/i3wm/script ~/.config/i3/
