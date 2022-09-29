f85d528124a8c21dfbdef36e3c2b301c23ad7c8f
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2022-09-29 17:28:01 +0900

    fix for chezmoi

---
+++ private_dot_local/private_share/chezmoi/install_scripts/executable_literal_run_once_install_font.sh
@@ -0,0 +1,5 @@
+#/usr/bin/bash
+
+sudo apt-get install -y fonts-font-awesome
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Roboto Mono Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf
